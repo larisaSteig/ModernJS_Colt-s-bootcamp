1bdac3a61222eef7d054ed59a8d6a149c2b9fe73
Larisa Steig <67192337+larisaSteig@users.noreply.github.com>
2020-10-12 05:54:51 -0600

    how to create a multiply Decks

--- DeckCard.js
+++ DeckCard.js
@@ -1,9 +1,9 @@
-function makeDeck(){
-  const values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']; // create an array
+function makeDeck() {
+  const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']; // create an array
   const suits = ["clubs", "spades", "hearts", "diamonds"];
-  const deck =[];
-  for(let value of values){
-    for(let suit of suits){
+  const deck = [];
+  for (let value of values) {
+    for (let suit of suits) {
       deck.push({
         value,
         suit
@@ -13,16 +13,16 @@ function makeDeck(){
   return deck
 }
 
-const myDeck={
+const myDeck = {
   deck: [],
-  suits:["clubs", "spades", "hearts", "diamonds"],
-  values:['2','3','4','5','6','7','8','9','10','J','Q','K','A'],
-  pickedCards:[],
-  originalDeck(){
+  suits: ["clubs", "spades", "hearts", "diamonds"],
+  values: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],
+  pickedCards: [],
+  originalDeck() {
     // deconstruct the object so you can use THIS
-    const{suits, values} = this;
-    for(let value of this.values){
-      for(let suit of this.suits){
+    const { suits, values } = this;
+    for (let value of this.values) {
+      for (let suit of this.suits) {
         this.deck.push({
           value,
           suit
@@ -31,30 +31,88 @@ const myDeck={
     }
     //return deck
   },
-  drawnCard(){
-    const{deck, pickedCards} = this;
+  drawnCard() {
+    const { deck, pickedCards } = this;
     const card = this.deck.pop()
     this.pickedCards.push(card)
     return card
   },
-  drawMultipleCard(num){
+  drawMultipleCard(num) {
     //empty array to see the picked cards
-   const cards =[];
-   for (let i=0; i<num; i++){
-     //calling the method above NUM amount of times
-     cards.push(this.drawnCard())
-   }
-   return cards;
+    const cards = [];
+    for (let i = 0; i < num; i++) {
+      //calling the method above NUM amount of times
+      cards.push(this.drawnCard())
+    }
+    return cards;
   },
-  shuffle(){
-    const {deck} = this;
+  shuffle() {
+    const { deck } = this;
     //loop over array backwards
-    for(let i = deck.length-1;i>0; i--){
+    for (let i = deck.length - 1; i > 0; i--) {
       // pick random index before element
-      let index = Math.floor(Math.random()* (i+1));
+      let index = Math.floor(Math.random() * (i + 1));
       //swap
-      [deck[i], deck[index]]=[deck[index], deck[i]];
+      [deck[i], deck[index]] = [deck[index], deck[i]];
     }
   }
 
 }
+//Defactoring the decks and making it in multiple way
+
+const makeDeck = () => {
+  return {
+    deck: [],
+    suits: ["clubs", "spades", "hearts", "diamonds"],
+    values: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],
+    pickedCards: [],
+    originalDeck() {
+      // deconstruct the object so you can use THIS
+      const { suits, values } = this;
+      for (let value of this.values) {
+        for (let suit of this.suits) {
+          this.deck.push({
+            value,
+            suit
+          })
+        }
+      }
+      //return deck
+    },
+    drawnCard() {
+      const { deck, pickedCards } = this;
+      const card = this.deck.pop()
+      this.pickedCards.push(card)
+      return card
+    },
+    drawMultipleCard(num) {
+      //empty array to see the picked cards
+      const cards = [];
+      for (let i = 0; i < num; i++) {
+        //calling the method above NUM amount of times
+        cards.push(this.drawnCard())
+      }
+      return cards;
+    },
+    shuffle() {
+      const { deck } = this;
+      //loop over array backwards
+      for (let i = deck.length - 1; i > 0; i--) {
+        // pick random index before element
+        let index = Math.floor(Math.random() * (i + 1));
+        //swap
+        [deck[i], deck[index]] = [deck[index], deck[i]];
+      }
+    }
+  }
+}
+//create one deck
+const deckOne = makeDeck();
+deckOne.originalDeck();
+deckOne.shuffle();
+const h1 = deckOne.drawMultipleCard(2);
+const h2 = deckOne.drawnCard();
+const h3  = deckOne.drawMultipleCard(3);
+
+//create a second deck
+const deckTwo = makeDeck();
